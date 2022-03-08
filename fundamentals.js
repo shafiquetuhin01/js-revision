38b8041916acad1e0c4b6064dd6ff68bcc862aa9
Shafique <sfqtuhin04@gmail.com>
2022-03-08 23:59:59 +0600

    fundamental updated

---
+++ fundamentals.js
@@ -0,0 +1,13 @@
+const student = {
+    name: 'Shafique',
+    age: 35,
+    choice: ['card', 'carom', 'cricket'],
+    child: {
+        first: 'nusair',
+        second: 'nahian',
+        third: ['niharika', 'naosara', 'neha', 'nowrin']
+    }
+}
+const myAge = 'age';
+console.log(student[myAge]);
+// console.log(student.child.third[2]);
